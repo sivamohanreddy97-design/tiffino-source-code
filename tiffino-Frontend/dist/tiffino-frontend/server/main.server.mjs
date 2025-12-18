474b1d40ff8701ed1882cec592b8739396c8d113
Ubuntu <ubuntu@ip-172-31-40-224.us-east-2.compute.internal>
2025-12-18 06:26:21 +0000

    update project files

---
+++ tiffino-Frontend/dist/tiffino-frontend/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-IZVLGL53.mjs";import"./chunk-WLVXZG7D.mjs";import"./chunk-7QACVLWC.mjs";import"./chunk-XXGBF7U6.mjs";import"./chunk-UOQXNTOC.mjs";import"./chunk-5NM7VLMJ.mjs";import"./chunk-VMNRQHPS.mjs";import"./chunk-VKAK7VJB.mjs";import"./chunk-GVJVC3KI.mjs";import"./chunk-FME56UVT.mjs";export{a as default};
---
+++ tiffino-Frontend/dist/tiffino-frontend/server/render-utils.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{g as o,h as r,i as n}from"./chunk-WLVXZG7D.mjs";import{Eb as e}from"./chunk-GVJVC3KI.mjs";import"./chunk-FME56UVT.mjs";export{n as renderApplication,r as renderModule,e as \u0275Console,o as \u0275SERVER_CONTEXT};
